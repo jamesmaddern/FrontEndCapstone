04bdef699b96a0d45bcce3dd5e84d2fbd3139509
James Maddern <jmaddern@srcuk.com>
2024-08-09 11:34:16 +0100

    improved navigation

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -2,27 +2,46 @@ import './App.css';
 import Nav from './Nav.js'
 import Main from './Main.js'
 import Footer from './Footer.js'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import BookingPage from './pages/BookingPage.js';
 import HomePage from './pages/HomePage.js';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 function App() {
   const aboutRef = useRef();
+  const navigate = useNavigate();
+  const [scrollToAbout, setScrollToAbout] = useState(false);
 
-  const scrollToAbout = () => {
-      aboutRef.current.scrollIntoView({ behavior: 'smooth' });
+  const handleScrollToAbout= () =>{
+    if (window.location.pathname !== '/'){
+      setScrollToAbout(true);
+      navigate('/');
+    } else {
+      aboutRef.current.scrollIntoView({behavior:'smooth'});
+    }
   };
+
+  useEffect(() => {
+    if (scrollToAbout && aboutRef.current){
+      aboutRef.current.scrollIntoView({ behavior:'smooth'});
+      setScrollToAbout(false);
+    }
+  }, [scrollToAbout, aboutRef]);
+
+
+
+
+
   return (
 
     <>
-      <BrowserRouter>
-      <Nav scrollToAbout={scrollToAbout}/>
+      
+      <Nav scrollToAbout={handleScrollToAbout}/>
         <Routes>
             <Route path="/" element={<HomePage aboutRef={aboutRef} />}></Route>
             <Route path="booking" element={<BookingPage />}></Route>
         </Routes>
       <Footer></Footer>
-      </BrowserRouter>
+     
     </>
   );
 }
